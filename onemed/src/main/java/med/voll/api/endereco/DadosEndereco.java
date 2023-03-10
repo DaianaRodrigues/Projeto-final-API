c15be8e140c84f399895caff095041f218aa5961
Daiana Rodrigues Nascimento <102737592+DaianaRodrigues@users.noreply.github.com>
2023-03-10 20:07:03 -0300

    Add files via upload

---
+++ onemed/src/main/java/med/voll/api/endereco/DadosEndereco.java
@@ -0,0 +1,18 @@
+package med.voll.api.endereco;
+
+public record DadosEndereco(
+
+        String logradouro,
+
+        String bairro,
+
+
+        String cep,
+
+        String cidade,
+
+        String uf,
+        String complemento,
+        String numero) {
+
+}
