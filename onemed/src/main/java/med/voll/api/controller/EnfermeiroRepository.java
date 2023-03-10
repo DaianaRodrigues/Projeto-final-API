cb5d3592b29e8d4a952569f6e8ba6dbb7defdc78
Daiana Rodrigues Nascimento <102737592+DaianaRodrigues@users.noreply.github.com>
2023-03-10 19:56:21 -0300

    Delete onemed directory

--- onemed/src/main/java/med/voll/api/controller/EnfermeiroRepository.java
+++
@@ -1,8 +0,0 @@
-package med.voll.api.controller;
-
-import med.voll.api.enfermeiro.Enfermeiro;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EnfermeiroRepository  extends JpaRepository<Enfermeiro, Long> {
-
-}
